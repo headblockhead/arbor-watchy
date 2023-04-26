652fda379d20e19db902464e804d7e5229cd0ec4
Edward Hesketh <headblockhead@gmail.com>
2023-04-26 18:01:43 +0100

    back to basics and added bluetoothoff icon

--- src/Watchy_Headblockhead.h
+++ src/Watchy_Headblockhead.h
@@ -2,21 +2,26 @@
 #define WATCHY_HEADBLOCKHEAD_H
 
 #include <Watchy.h>
-#include "Seven_Segment10pt7b.h"
-#include "DSEG7_Classic_Regular_15.h"
+
 #include "DSEG7_Classic_Bold_25.h"
+#include "DSEG7_Classic_Regular_15.h"
 #include "DSEG7_Classic_Regular_39.h"
+#include "Seven_Segment10pt7b.h"
 #include "icons.h"
 
-class WatchyHeadblockhead : public Watchy{
-    using Watchy::Watchy;
-    public:
-        void drawWatchFace();
-        void drawTime();
-        void drawDate();
-        void drawSteps();
-        void drawWeather();
-        void drawBattery();
+class WatchyHeadblockhead : public Watchy {
+  using Watchy::Watchy;
+
+public:
+  void drawWatchFace();
+  void drawTime();
+  void drawDate();
+  void drawSteps();
+  void drawWeather();
+  void drawBattery();
+  void drawWifi(int x, int y);
+  void drawBluetooth(int x, int y);
+  virtual void handleButtonPress(); // override default button press handling
 };
 
 #endif
